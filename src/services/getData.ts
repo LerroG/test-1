976e005d308b8e96a3ceb305132470f72035ed7c
Ruslan <ssakoks@mail.ru>
2024-08-19 08:15:00 +0500

    fix

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -4,7 +4,10 @@ import axios from "axios";
 class DataService {
   async getData() {
     const { data } = await axios.get<IFullResponse>(
-      "http://194.93.27.27/TestReporting/Reporting.svc/json/GetBranchReport?branchIds=2011,2012,2013,2015,2014,2016,2017,2018,2019,2020&startDate=2024-05-01&endDate=2024-05-31"
+      "http://194.93.27.27/TestReporting/Reporting.svc/json/GetBranchReport?branchIds=2011,2012,2013,2015,2014,2016,2017,2018,2019,2020&startDate=2024-05-01&endDate=2024-05-31",
+      {
+        headers: { "Content-type": "application/json" },
+      }
     );
 
     return data;
